b073a325bdbbcde359fad1f0490ac0a2a423be70
Maxim Pervushin <exxenos@gmail.com>
2012-11-27 15:21:18 +0400

    Initial Commit

---
+++ ComplexStatusItem/MFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFAppDelegate.h
+//  ComplexStatusItem
+//
+//  Created by Maxim Pervushin on 11/27/12.
+//  Copyright (c) 2012 Maxim Pervushin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ComplexStatusItem/MFAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MFAppDelegate.m
+//  ComplexStatusItem
+//
+//  Created by Maxim Pervushin on 11/27/12.
+//  Copyright (c) 2012 Maxim Pervushin. All rights reserved.
+//
+
+#import "MFAppDelegate.h"
+
+@implementation MFAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
